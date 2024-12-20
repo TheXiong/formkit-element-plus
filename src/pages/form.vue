69f5144c2abc53b935af485b1ad03d57e4391630
xiong7 <578418374@qq.com>
2024-12-20 10:39:09 +0800

    feat: 测试

--- src/pages/form.vue
+++ src/pages/form.vue
@@ -22,75 +22,44 @@ const submit = (a) => {
 
 }
 
-const schema = [
+const columns = [
     {
-        $formkit: "el-input",
-        name: 'dfd',
-        label: `hhsdfdf`,
-        id: 'dfd'
+        $formkit: "el-switch",
+        name: "negate",
+        label: `Not`,
+        inlinePrompt: true,
+        activeIcon: Check,
+        inactiveIcon: Close,
+        value: true,
+        children: ["Not"],
+        columnWidth: '100px'
     },
     {
-        $formkit: "el-array-table",
-        name: "filters",
-        label: `关联筛选器`,
-        dynamic: true,
-        upControl: false,
-        downControl: false,
-        insertControl: false,
-        removeControl: true,
-        addLabel: "添加筛选器",
-        actionWidth: '200px',
-        controlAttrs: {
-            type: "primary",
-            icon: Check
-        },
-        value: [
-            {
-                negate: true,
-                relationType: 'aaa',
-                entityTypes: 'DEVICE'
-            }
+        $formkit: "el-select",
+        name: "relationType",
+        label: `关联类型`,
+    },
+    {
+        $formkit: "el-select",
+        name: "entityTypes",
+        label: "实体类型",
+        value: "",
+        options: [
+            { label: "设备", value: 'DEVICE' },
+            { label: "资产", value: 'ASSET' },
+            { label: "实体视图", value: 'ENTITY_VIEW' },
+            { label: "租户", value: 'TENANT' },
+            { label: "客户", value: 'CUSTOMER' },
+            { label: "用户", value: 'USER' },
         ],
-        columns: [
-          {
-            $formkit: "el-switch",
-            name: "negate",
-            label: `Not`,
-            inlinePrompt: true,
-            activeIcon: Check,
-            inactiveIcon: Close,
-            value: true,
-            children: ["Not"],
-            columnWidth: '100px'
-          },
-          {
-            $formkit: "el-select",
-            name: "relationType",
-            label: `关联类型`,
-          },
-          {
-            $formkit: "el-select",
-            name: "entityTypes",
-            label: "实体类型",
-            value: "",
-            options: [
-              { label: "设备", value: 'DEVICE' },
-              { label: "资产", value: 'ASSET' },
-              { label: "实体视图", value: 'ENTITY_VIEW' },
-              { label: "租户", value: 'TENANT' },
-              { label: "客户", value: 'CUSTOMER' },
-              { label: "用户", value: 'USER' },
-            ],
-          },
-        ],
-      }
+    },
 ]
 </script>
 <template>
     <pre wrap>{{ data }}</pre>
 
     <FormKit ref="myForm" type="elForm" v-model="data" @submit="onClickSubmit" label-position="top">
-        <el-tabs model-value="first" class="demo-tabs" @tab-click="handleClick">
+        <el-tabs model-value="first" class="demo-tabs">
             <el-tab-pane label="User" name="first">
                 <FormKit type="el-input" label="aaa" name="aaa" validation="required" formItem />
             </el-tab-pane>
@@ -98,7 +67,10 @@ const schema = [
                 <FormKit type="el-input" label="bbb" name="bbb" validation="required" formItem />
             </el-tab-pane>
         </el-tabs>
-        <FormKitSchema :schema="schema"></FormKitSchema>
+        <FormKit type="el-array-table" name="filters" label='关联筛选器' :dynamic="true" :upControl="false"
+            :downControl="false" :insertControl="false" :removeControl="true" addLabel="添加筛选器" actionWidth='200px'
+            :columns="columns">
+        </FormKit>
 
         <FormKit type="el-input" id="name" label="Name abcdef" name="name" validation="required" formItem>
             <!-- sds -->
