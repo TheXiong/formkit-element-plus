acb89f6f237a9e53b42258bba9b1bea206ffd630
xiong7 <578418374@qq.com>
2025-07-24 18:28:44 +0800

    fix: 修复computedValuePlugin导致回显错误

--- src/components/plugins/computedValuePlugin.ts
+++ src/components/plugins/computedValuePlugin.ts
@@ -1,26 +1,26 @@
 import { FormKitNode, FormKitPlugin } from "@formkit/core";
 
-export function createComputedValuePlugin(
-    options?: any
-): FormKitPlugin {
-    const computedValuePlugin = (node: FormKitNode) => {
-        // Dont run this for groups/lists:
-        // if (node.type !== 'input') return
+export function createComputedValuePlugin(): FormKitPlugin {
+  const computedValuePlugin = (node: FormKitNode) => {
+    // Dont run this for groups/lists:
+    // if (node.type !== 'input') return
 
-        // Add our special prop to make it reactive
-        node.addProps(['computedValue'])
+    // Add our special prop to make it reactive
+    node.addProps(["computedValue"]);
 
-        // If the intitial value is not the same as the computed one,
-        // then set it.
-        if (typeof node.props.computedValue !== 'undefined' && node.props.computedValue !== node._value) {
-            node.input(node.props.computedValue)
-        }
+    node.on("prop:computedValue", ({ payload }) => {
+      // if the prop `computedValue` changes, set that as the
+      // value of the input.
+      node.input(payload);
+    });
 
-        node.on('prop:computedValue', ({ payload }) => {
-            // if the prop `computedValue` changes, set that as the
-            // value of the input.
-            node.input(payload)
-        })
-    }
-    return computedValuePlugin
+    node.on("mounted", () => {
+      // If the intitial value is not the same as the computed one,
+      // then set it.
+      if (typeof node.props.computedValue !== "undefined" && !node._value) {
+        node.input(node.props.computedValue);
+      }
+    });
+  };
+  return computedValuePlugin;
 }
